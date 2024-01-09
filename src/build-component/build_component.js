02c0e88d0d338023b661b76652869abfc4e9ae22
Shaik Sameera <shaiksameera156@gmail.com>
2024-01-09 08:58:45 +0530

    Changes

---
+++ src/build-component/build_component.js
@@ -0,0 +1,36 @@
+function Myfisrtapp()
+{
+  return (
+    <div>
+   <table>
+    <tr>
+      <th>column1</th>
+      <th>column2</th>
+      <th>column3</th>
+    </tr>
+    <tr>
+      <td>sub1</td>
+      <td>sub2</td>
+      <td>sub3</td>
+    </tr>
+    <tr>
+      <td>sub4</td>
+      <td>sub5</td>
+      <td>sub6</td>
+    </tr>
+   </table>
+   <ul>
+    <li>list1</li>
+    <li>list2</li>
+    <li>list3</li>
+   </ul>
+   <button type="button">ok</button>
+   <form>
+    <label for="em">EmailId:</label>
+    <input type="email" id="em"></input>
+   </form>
+   </div>
+  
+  )
+}
+export default Myfisrtapp;
